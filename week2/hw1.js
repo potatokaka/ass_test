185ff8b8160c7904072c503ce9e4212a41adb9d1
Kaka <potatokaka@gmail.com>
2019-04-25 20:36:33 +0800

    week2

---
+++ week2/hw1.js
@@ -0,0 +1,7 @@
+function printStars(n) {
+  for(var i=1; i <= n ; i++){
+    console.log('*')
+  }
+}
+
+printStars(5);
---
+++ week2/hw2.js
@@ -0,0 +1,6 @@
+function capitalize(str) {
+  var result = str[0].toUpperCase() + str.slice(1);
+    return result;
+}
+
+console.log(capitalize('hello'));
