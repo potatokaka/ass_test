2714523ba0224769c725b684e9f584ed80e83dbb
Kaka <potatokaka@gmail.com>
2019-09-02 17:34:20 +0800

    week20

---
+++ week19/hw2/conn_AWS.php
@@ -0,0 +1,21 @@
+<?php
+  $servername = 'localhost';
+  $username = 'root';
+  $password = 'dididd177'; //預設是沒密碼
+  $dbname = 'mentor_2019';
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  //編碼：執行一個 query，才不會中文變亂碼
+  $conn->query('SET NAMES UTF8');
+  $conn->query("SET time_zone = '+08:00'");
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+?>
+
+
